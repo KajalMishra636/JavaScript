2657f2f110ef9a615704e6d884c127a537ce8431
Kajal Mishra <kajalmishraa345@gmail.com>
2025-08-19 15:34:26 +0000

    datatype, variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId= 23345
+let accountEmail = "kajal@google.com"
+var accountPassword = "12466"
+accountCity = "Jaipur"
+
+
+// accountId = 2  //not allowed 
+
+accountEmail = "hc@hs.com"
+accountPassword = "635487"
+accountCity = "Goa"
+
+console.log(accountId);
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,26 @@
+"use strict";    //treat all JS code as newer version 
+
+
+//  alert ("hello");  
+
+console.log("hello");
+
+let name = "kajal"
+let age = 18 
+let isLoggedIn = false
+let  state;
+
+//number => 2 to power 53 
+//bigint 
+//string  => ""
+//boolean => true/false
+//null => standalone value 
+//undefined => 
+//symbol  => unique 
+
+
+// objects 
+console.log(typeof null);  // object 
+console.log(typeof undefined);  // undefined 
+
+
